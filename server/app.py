574e9b69e42eb430631878cade9e56bc1c2d1ac6
brewchetta <chett.tiller@gmail.com>
2023-05-03 09:19:39 -0400

    init

---
+++ server/app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+from models import db # import your models here!
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+@app.get('/')
+def index():
+    return "Hello world"
+
+# write your routes here!
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
